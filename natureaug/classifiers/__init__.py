4c8c7af68cdd4cf2da7053a7051bbffbdd70da48
maxporyvay <sevmaxclever@gmail.com>
2025-05-08 21:43:01 +0300

    Small fix for inception_v3 model

--- natureaug/classifiers/__init__.py
+++ natureaug/classifiers/__init__.py
@@ -3,12 +3,13 @@ from functools import partial
 from torchvision.models import alexnet, resnet18, vgg11, inception_v3
 
 setting_no_weights = lambda classifier: partial(classifier, weights=None)
+disabling_inception_aux_logits = lambda classifier: partial(classifier, aux_logits=False)
 
 CLASSIFIERS = {
     'alexnet': setting_no_weights(alexnet),
     'resnet18': setting_no_weights(resnet18),
     'vgg11': setting_no_weights(vgg11),
-    'inception_v3': setting_no_weights(inception_v3),
+    'inception_v3': setting_no_weights(disabling_inception_aux_logits(inception_v3)),
 }
 
 
